da548fc8a956edee6384656aaf7336497feedf81
Paweł Świątkowski <pawel.swiatkowski@fyber.com>
2016-12-10 22:30:43 +0100

    day six

---
+++ 06 [Nim]/step_1.nim
@@ -0,0 +1,29 @@
+import tables
+import os
+import streams
+import strutils
+
+let arguments = commandLineParams()
+let fname = arguments[0]
+
+var stream = newFileStream(fname)
+let firstLine = stream.readLine()
+let length = firstLine.len() - 1
+stream.setPosition(0)
+var resultArray = newTable[int, CountTable[char]]()
+
+for i in 0..length:
+  resultArray[i] = initCountTable[char]()
+
+while not stream.atEnd:
+  let line = stream.readLine()
+  for i, c in line:
+    resultArray[i].inc(c)
+
+var result = "";
+for i in 0..length:
+  result = result & resultArray[i].largest().key
+
+echo result
+
+stream.close()
